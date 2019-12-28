005acdf3062574b0bb0bc2f92f1947003b5a53e4
Tillmann Heidsieck <theidsieck@leenox.de>
2019-12-28 06:39:13 +0100

    clean up RFID reader/writer code
    Signed-off-by: Tillmann Heidsieck <theidsieck@leenox.de>

--- Tonuino/RFIDReader.ino
+++ Tonuino/RFIDReader.ino
@@ -18,7 +18,6 @@ RFIDReader::RFIDReader(MFRC522 *mfrc522) : mfrc522(mfrc522) {
 
 int RFIDReader::write(RFIDCard *card) {
     MFRC522::StatusCode status;
-    MFRC522::PICC_Type mifareType;
 
     byte buffer[16] = {
         card->chip_id[0], card->chip_id[1], card->chip_id[2], card->chip_id[3],
@@ -31,32 +30,22 @@ int RFIDReader::write(RFIDCard *card) {
     Serial.print(F("Card UID:"));
     dump_byte_array(mfrc522->uid.uidByte, mfrc522->uid.size);
 
-    Serial.println();
     Serial.print(F("PICC type: "));
     MFRC522::PICC_Type piccType = mfrc522->PICC_GetType(mfrc522->uid.sak);
     Serial.println(mfrc522->PICC_GetTypeName(piccType));
 
-    // Authenticate using key B
     Serial.println(F("Authenticating again using key B..."));
     status = mfrc522->PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_B, trailerBlock, &keyB, &(mfrc522->uid));
     if (status != MFRC522::STATUS_OK) {
         Serial.print(F("PCD_Authenticate() failed: "));
         Serial.println(mfrc522->GetStatusCodeName(status));
-        tonuino->get_dfplayer()->playMp3FolderTrack(MESSAGE_ERROR);
-        delay(200);
         return -1;
     }
 
     status = mfrc522->MIFARE_Write(blockAddr, buffer, 16);
     if (status != MFRC522::STATUS_OK) {
         Serial.print(F("MIFARE_Write() failed: "));
-        Serial.println(mfrc522->GetStatusCodeName(status));
-        tonuino->get_dfplayer()->playMp3FolderTrack(MESSAGE_ERROR);
-        delay(200);
         return -1;
-    } else {
-        tonuino->get_dfplayer()->playMp3FolderTrack(MESSAGE_CARD_CONFIGURED);
-        delay(200);
     }
     Serial.println();
 
@@ -65,57 +54,44 @@ int RFIDReader::write(RFIDCard *card) {
 
 int RFIDReader::read(RFIDCard *card) {
     MFRC522::StatusCode status;
-    bool returnValue = true;
     byte buffer[18], size = 18;
 
-    // Show some details of the PICC (that is: the tag/card)
     Serial.print(F("Card UID:"));
     dump_byte_array(mfrc522->uid.uidByte, mfrc522->uid.size);
 
-    Serial.println();
     Serial.print(F("PICC type: "));
     MFRC522::PICC_Type piccType = mfrc522->PICC_GetType(mfrc522->uid.sak);
     Serial.println(mfrc522->PICC_GetTypeName(piccType));
 
-    // Authenticate using key A
     Serial.println(F("Authenticating using key A..."));
     status = mfrc522->PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, trailerBlock, &keyA, &(mfrc522->uid));
     if (status != MFRC522::STATUS_OK) {
-        returnValue = false;
         Serial.print(F("PCD_Authenticate() failed: "));
         Serial.println(mfrc522->GetStatusCodeName(status));
-        return returnValue;
+        return -1;
     }
 
-    // Show the whole sector as it currently is
     Serial.println(F("Current data in sector:"));
     mfrc522->PICC_DumpMifareClassicSectorToSerial(&(mfrc522->uid), &keyA, sector);
     Serial.println();
 
     status = mfrc522->MIFARE_Read(blockAddr, buffer, &size);
     if (status != MFRC522::STATUS_OK) {
-        returnValue = false;
         Serial.print(F("MIFARE_Read() failed: "));
         Serial.println(mfrc522->GetStatusCodeName(status));
+        return -1;
     }
 
-    card->chip_id[0] = buffer[0x0];
-    card->chip_id[1] = buffer[0x1];
-    card->chip_id[2] = buffer[0x2];
-    card->chip_id[3] = buffer[0x3];
+    for (size = 0; size < 4; size++)
+        card->chip_id[size] = buffer[size];
+
+    for (size = 0; size < 10; size++)
+        card->extdata[size] = buffer[size];
+
     card->version    = buffer[0x4];
     card->card_mode  = buffer[0x5];
-    card->extdata[1] = buffer[0x7];
-    card->extdata[2] = buffer[0x8];
-    card->extdata[3] = buffer[0x9];
-    card->extdata[4] = buffer[0xa];
-    card->extdata[5] = buffer[0xb];
-    card->extdata[6] = buffer[0xc];
-    card->extdata[7] = buffer[0xd];
-    card->extdata[8] = buffer[0xe];
-    card->extdata[9] = buffer[0xf];
 
-    return returnValue;
+    return 0;
 }
 
 void RFIDReader::setup(TonUINO *tonuino) {
@@ -136,7 +112,14 @@ void RFIDReader::loop() {
         return;
 
     RFIDCard *card = new RFIDCard(this);
-    read(card);
+    if (!card)
+        return;
+
+    if (!card->read()) {
+        delete card;
+        return;
+    }
+
     tonuino->notify_rfid(card);
 }
 
@@ -152,5 +135,4 @@ void RFIDReader::dump_byte_array(byte *buffer, byte bufferSize) {
     }
     Serial.println();
 }
-
 // vim: ts=4 sw=4 et cindent
--- Tonuino/rfid.h
+++ Tonuino/rfid.h
@@ -49,8 +49,8 @@ public:
     uint8_t card_mode;
     uint8_t extdata[10];
 
-    void write() {reader->write(this);}
-    void read()  {reader->read(this);}
+    bool write() {return reader->write(this) >= 0;}
+    bool read()  {return reader->read(this) >= 0;}
     bool check(uint32_t chip_id) {
         return this->chip_id[0] == ((chip_id >>  0) & 0xff) &&
             this->chip_id[1] == ((chip_id >>  8) & 0xff) &&
