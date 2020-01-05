64d01f401074053a2fadb6d37353928197f28eaa
Tillmann Heidsieck <theidsieck@leenox.de>
2020-01-05 21:50:36 +0100

    tools: add gcloud speech synth
    Signed-off-by: Tillmann Heidsieck <theidsieck@leenox.de>

--- Tools/create-soundfiles.sh
+++ Tools/create-soundfiles.sh
@@ -1,9 +1,47 @@
 #!/bin/bash
 
+TTS=${3:-tts_gcloud}
 LANG=${2:-de}
 OUT=${1:-mp3}
+
+LANG_CODE="de-DE"
+
+case $LANG in
+    "de")
+        LANG_CODE="de-DE"
+        ;;
+    "en")
+        LANG_CODE="en-US"
+        ;;
+    *)
+        echo "unknown language"
+        exit -1
+        ;;
+esac
+
 BASE=$(dirname $0)
 TMP=$(mktemp -d)
+
+tts_gcloud() {
+    if [[ -z "$GOOGLE_APPLICATION_CREDENTIALS" ]]; then
+        echo "need to set GOOGLE_APPLICATION_CREDENTIALS"
+        exit -1
+    fi
+
+    curl -s -H "Authorization: Bearer $(gcloud auth application-default print-access-token)" \
+        -H "Content-Type: application/json; charset=utf-8" \
+        --data "{'input':{'text':'${1}'},
+        'voice':{
+          'languageCode':'${LANG_CODE}',
+          'name':'${LANG_CODE}-Wavenet-A',
+          'ssmlGender':'FEMALE'
+        },
+        'audioConfig':{
+          'audioEncoding':'MP3'
+        }
+      }" "https://texttospeech.googleapis.com/v1/text:synthesize" > $2.txt
+    cat $2.txt | jq .audioContent | sed -e 's/^"\|"$//g' | base64 --decode > $2.mp3
+}
 
 tts_google() {
     gtts-cli -l ${LANG} -o ${2}.mp3 "${1}"
@@ -16,8 +54,14 @@ tts_sox() {
 }
 
 for i in {1..250}; do
-    echo "${i} \"${i}\""
-#    tts_google "$i" ${TMP}/$(printf "%04d" $i)
+    out=$(printf "%04d" $i)
+    echo -n "${out} "
+    if [[ -f ${OUT}/${out} ]]; then
+        echo "skipping"
+        continue
+    fi
+    ${TTS} "$i" ${TMP}/${out}
+    echo "done"
 done
 
 gcc -I ${BASE}/../Tonuino -E -x c++ -w -P \
@@ -26,9 +70,16 @@ gcc -I ${BASE}/../Tonuino -E -x c++ -w -P \
 while read f; do
     i=$(echo -ne "$f" | awk -F ';' '{print $1}')
     t=$(echo -ne "$f" | awk -F ';' '{print $2}' | sed -e 's/"//g')
-    echo "$i $t"
-    tts_google "$t" ${TMP}/$(printf "%04d" $i)
+    out=$(printf "%04d" $i)
+    echo -n "$out "
+    if [[ -f ${OUT}/${out} ]]; then
+        echo "skipping"
+        continue
+    fi
+    ${TTS} "$t" ${TMP}/$out
+    echo "done"
 done < <(cat ${TMP}/soundfiles.txt)
 
 mkdir -p ${OUT}
 mv ${TMP}/*.mp3 ${OUT}/
+rm -rf ${TMP}
