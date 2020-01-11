67b45e77d748b2eb6619dd4cdb5235d4efa97cba
Tillmann Heidsieck <theidsieck@leenox.de>
2020-01-11 06:26:04 +0100

    mod: LockKeys: return state
    Signed-off-by: Tillmann Heidsieck <theidsieck@leenox.de>

--- Tonuino/Mod_LockKeys.ino
+++ Tonuino/Mod_LockKeys.ino
@@ -6,18 +6,20 @@
 #include "modificator.h"
 
 TState *Modificator_LockKeys::loop()  {
-
+    return state;
 }
 
 TState *Modificator_LockKeys::handle_buttons(uint32_t &_map)  {
 	_map = 0;
+    return state;
 }
 
 TState *Modificator_LockKeys::handle_card(RFIDCard *card)  {
+    return state;
 }
 
 TState *Modificator_LockKeys::handle_dfplay_event(mp3_notify_event &event, uint16_t &code)  {
-
+    return state;
 }
 
 Modificator_LockKeys::Modificator_LockKeys(TonUINO *context, TState *state) {
